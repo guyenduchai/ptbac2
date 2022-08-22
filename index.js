cc3709ce8508cf4b57a95d63fadaf6aad76372ad
guyenduchai <109466706+guyenduchai@users.noreply.github.com>
2022-08-22 22:40:50 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function pt2(a , b ,c ){
+function pt2(a, b ,c ){
 	if (a == 0){
 		if (b==0){
 			console.log("vo so nghiem");
@@ -7,17 +7,20 @@ function pt2(a , b ,c ){
 		console.log("x =", -c/b);
 		return;
 	}
-	const delta=Math.sqrt(b*b -4*a*c);
+	const delta=b*b - 4*a*c;
+	console.log(delta);
 	if (delta < 0){
 		console.log("vo nghiem");
 		return;
 	}
 	if (delta ==0){
 		console.log("x=",-b/(2*a));
+		return;
 	}
 	if (delta > 0){
 		console.log("x1 =",(-b+Math.sqrt(delta))/(2*a));
 		console.log("x2 =",(-b-Math.sqrt(delta))/(2*a));
+		return;
 	}
 }
-pt2("1",2,null)
+pt2(1,2,1)
